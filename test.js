fe1f4ad194343099096e2a13f8bc9cc59b9fb6cb
“Kirti <kirti.kaushal@gmail.com>
2023-08-05 13:39:33 -0400

    Copilot functions

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
-const namekirti ="kirti"
+const firstname ="kirti"
 
-console.log(namekirti , 'my name');
+console.log(firstname , 'my name');
+
+function add(a,b){
+    return a+b;
+}
+
+function deleteNumber(a,b){
+    return a-b;
+}
